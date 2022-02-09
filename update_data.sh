9a2efe67cca682930a070577719854fd8dba1508
Michael Gollnick <mollnick@gmx.net>
2022-02-09 20:23:08 +0100

    Fix the script to upload the data file
    Use the FX tools under ./lib to upload the data file.

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-./create_data.sh 
-cp data-output/Vault83.bin ../../Arduino/Arduboy_FX/Arduboy-Python-Utilities/example-flashcart/FPS/Vault83Data.bin
-cd ../../Arduino/Arduboy_FX/Arduboy-Python-Utilities
-python flashcart-writer.py -d example-flashcart/FPS/Vault83Data.bin 
-cd -
+python ./lib/Arduboy-Python-Utilities/flashcart-writer.py -d data-output/Vault83.bin
