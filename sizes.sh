b8f2391cd49485be245c2305bb0f2ad85e2fc952
Michael Gollnick <mollnick@gmx.net>
2022-03-21 20:08:06 +0100

    fix sizes script to use the correct .elf file

--- sizes.sh
+++ sizes.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-avr-objdump -t .pio/build/leonardo/firmware.elf -C  |sort -k4 | uniq | awk  '{ $5 = sprintf("%d:", "0x" $5) } 1'
+avr-objdump -t .pio/build/FXDemoPlatform/firmware.elf -C  |sort -k4 | uniq | awk  '{ $5 = sprintf("%d:", "0x" $5) } 1'
 
